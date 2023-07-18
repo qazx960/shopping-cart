d6f2ffe7205972b3451df2512e6bce5b0a38ce22
Jiwon Lee <giwon.yi339@gmail.com>
2023-07-18 16:47:32 +0900

    Header UI creation

---
+++ src/component/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./header.css";
+
+export const Header = () => {
+  return (
+    <nav className="navigation__bar">
+      <div>
+        <h2>Shop Cart Project</h2>
+      </div>
+      <div>
+        <h2>Cart</h2>
+      </div>
+    </nav>
+  );
+};
