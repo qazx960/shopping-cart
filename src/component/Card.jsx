0b97d94a8eff909302cf2cacd534dd4654cef8ac
Jiwon Lee <giwon.yi339@gmail.com>
2023-07-18 16:56:50 +0900

    Card component created

---
+++ src/component/Card.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import "./card.css";
+
+export const Card = () => {
+  return <div className="card"></div>;
+};
--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import "./main.css";
+import { Card } from "../component/Card";
 
 export const MainPage = () => {
   return (
     <main>
-      <div></div>
+      <section>
+        <h1>Products</h1>
+        <Card />
+      </section>
     </main>
   );
 };
