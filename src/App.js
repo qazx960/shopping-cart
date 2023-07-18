14fe0bc16fac4f677fe8861be75e3895724cbab1
Jiwon Lee <giwon.yi339@gmail.com>
2023-07-18 17:07:28 +0900

    fetch data setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
+import { useEffect, useState } from "react";
 import { Header } from "./component/Header";
 import { MainPage } from "./pages/MainPage";
 
 function App() {
+  const [cartItem, setCartItem] = useState([]);
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/products?limit=10")
+      .then((res) => res.json())
+      .then((data) => {
+        setCartItem(data);
+      });
+  }, []);
+
   return (
     <>
       <Header />
-      <MainPage />
+      <MainPage cartItem={cartItem} />
     </>
   );
 }
--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./main.css";
 import { Card } from "../component/Card";
 
-export const MainPage = () => {
+export const MainPage = ({ cartItem }) => {
+  //   console.log(cartItem);
+
   return (
     <main>
       <section>
