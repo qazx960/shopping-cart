1f3ef5881f621d88e5f43bedfa47e1c5a27ebc79
Jiwon Lee <giwon.yi339@gmail.com>
2023-07-18 17:34:55 +0900

    add to cart popup created

--- src/component/Card.jsx
+++ src/component/Card.jsx
@@ -1,21 +1,35 @@
 import React, { useState } from "react";
 import "./card.css";
+import "./popup.css";
 
 export const Card = ({ cartItem }) => {
   const [addProduct, setAddProduct] = useState(false);
   return (
-    <div className="card__list">
-      {cartItem.map((item) => (
-        <div className="card" key={item.id}>
-          <img src={item.image} alt="img" className="card__img" />
-          <h4>{item.title}</h4>
-          <p>{item.price}</p>
-          <p>{item.category}</p>
-          <button onClick={() => setAddProduct(!addProduct)}>
-            Add to Cart
-          </button>
-        </div>
-      ))}
+    <section>
+      <div className="card__list">
+        {cartItem.map((item) => (
+          <div className="card" key={item.id}>
+            <img src={item.image} alt="img" className="card__img" />
+            <h4>{item.title}</h4>
+            <p>{item.price}</p>
+            <p>{item.category}</p>
+            <button onClick={() => setAddProduct(!addProduct)}>
+              Add to Cart
+            </button>
+          </div>
+        ))}
+      </div>
+      {addProduct && <DisplayPopup />}
+    </section>
+  );
+};
+
+export const DisplayPopup = () => {
+  return (
+    <div className="popup">
+      <ul className="popup__list">
+        <li className="popup__item">Added to Cart!</li>
+      </ul>
     </div>
   );
 };
--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,14 +1,19 @@
 import React from "react";
 import "./header.css";
+import { Link } from "react-router-dom";
 
 export const Header = () => {
   return (
     <nav className="navigation__bar">
       <div>
-        <h2>Shop Cart Project</h2>
+        <Link to="/">
+          <h2>Shop Cart Project</h2>
+        </Link>
       </div>
       <div>
-        <h2>Cart</h2>
+        <Link to="/cart">
+          <h2>Cart</h2>
+        </Link>
       </div>
     </nav>
   );
