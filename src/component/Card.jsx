52cec046d24f1650b917ebf839142a1b271e2d23
Jiwon Lee <giwon.yi339@gmail.com>
2023-07-18 17:14:20 +0900

    data prop drilling into card component and rendered items

--- src/component/Card.jsx
+++ src/component/Card.jsx
@@ -1,14 +1,21 @@
 import React, { useState } from "react";
 import "./card.css";
 
-export const Card = () => {
+export const Card = ({ cartItem }) => {
   const [addProduct, setAddProduct] = useState(false);
   return (
-    <div className="card">
-      <img src="" alt="img" />
-      <p>Title</p>
-      <p>price :</p>
-      <button onClick={() => setAddProduct(!addProduct)}>Add to Cart</button>
+    <div className="card__list">
+      {cartItem.map((item) => (
+        <div className="card" key={item.id}>
+          <img src={item.image} alt="img" className="card__img" />
+          <h4>{item.title}</h4>
+          <p>{item.price}</p>
+          <p>{item.category}</p>
+          <button onClick={() => setAddProduct(!addProduct)}>
+            Add to Cart
+          </button>
+        </div>
+      ))}
     </div>
   );
 };
--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -3,13 +3,11 @@ import "./main.css";
 import { Card } from "../component/Card";
 
 export const MainPage = ({ cartItem }) => {
-  //   console.log(cartItem);
-
   return (
     <main>
       <section>
         <h1>Products</h1>
-        <Card />
+        <Card cartItem={cartItem} />
       </section>
     </main>
   );
